2873aff65e74d5a762637d29ac072964d73c9eae
Oyekunle-Mark <oyekunlemac@gmail.com>
2018-12-12 16:24:45 +0100

    Express static file app

---
+++ README.md
@@ -0,0 +1,3 @@
+An express static file server.
+
+The logging and file server middleware are exclusive
---
+++ index.js
@@ -0,0 +1,41 @@
+// require the dependencies
+let express = require('express');
+let fs = require('fs');
+let path = require('path');
+
+//create an express app
+let app = express();
+
+//add the logging middleware to the express middleware stack
+app.use(function(req, res, next) {
+	console.log('Request IP: ' + req.url);
+	console.log('Request date: ' + new Date());
+	next();		//pass control to the next middleware
+});
+
+//the middleware to serve the file
+app.use(function(req, res, next) {
+	let filePath = path.join(__dirname, "static", req.url);
+	fs.stat(filePath, function(err, fileInfo) {
+		if (err) {	//if file does not exist
+			next();
+			return;
+		}
+
+		if (fileInfo.isFile()) {
+			res.sendFile(filePath);
+		} else {
+			next();
+		}
+	});
+});
+
+//the error handling middleware
+app.use(function(req, res) {
+	res.status(404).send('File not found!');
+});
+
+//start the express app
+app.listen(3000, function() {
+	console.log('The file server  has started on port 3000.');
+});
